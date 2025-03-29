19dbf8dd45950ac95d5cc9b674ec3e4f7e7ad5f9
NguyenThaiDucTimshel <nguyenthaiduc@yahoo.com>
2025-03-29 16:41:14 +0700

    dotfiles

---
+++ fish/aliases/main.fish
@@ -0,0 +1,130 @@
+
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+#list
+alias ls='ls --color=auto'
+alias la='ls -a'
+alias ll='ls -la'
+alias l='ls'
+alias l.="ls -A | egrep '^\.'"
+
+#fix obvious typo's
+alias cd..='cd ..'
+alias pdw="pwd"
+alias udpate='sudo pacman -Syyu'
+alias upate='sudo pacman -Syyu'
+alias updte='sudo pacman -Syyu'
+alias updqte='sudo pacman -Syyu'
+
+## Colorize the grep command output for ease of use (good for log files)##
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+#readable output
+alias df='df -h'
+
+#pacman unlock
+alias unlock="sudo rm /var/lib/pacman/db.lck"
+alias rmpacmanlock="sudo rm /var/lib/pacman/db.lck"
+
+#free
+alias free="free -mt"
+
+#continue download
+alias wget="wget -c"
+
+#userlist
+alias userlist="cut -d: -f1 /etc/passwd"
+
+#merge new settings
+alias merge="xrdb -merge ~/.Xresources"
+
+# Aliases for software managment
+# pacman or pm
+alias pacman='sudo pacman --color auto'
+alias update='sudo pacman -Syyu;yay -Su'
+
+#ps
+alias psa="ps auxf"
+alias psgrep="ps aux | grep -v grep | grep -i -e VSZ -e"
+
+#grub update
+alias update-grub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
+
+#switch between bash and zsh
+alias tobash="sudo chsh $USER -s /bin/bash && echo 'Now log out.'"
+alias tozsh="sudo chsh $USER -s /bin/zsh && echo 'Now log out.'"
+alias tofish="sudo chsh $USER -s /bin/fish && echo 'Now log out.'"
+
+#hardware info --short
+alias hw="hwinfo --short"
+alias hyc="hyprctl clients | grep 'initialClass'"
+
+
+#check vulnerabilities microcode
+alias microcode='grep . /sys/devices/system/cpu/vulnerabilities/*'
+
+#get fastest mirrors in your neighborhood
+alias mirror="sudo reflector  -c VN,SG,HK -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
+alias mirrord="sudo cachyos-rate-mirrors"
+alias mirrors="sudo reflector -c VN,SG,HK --latest 30 --number 10 --sort score --save /etc/pacman.d/mirrorlist"
+alias mirrora="sudo reflector -c VN,SG,HK --latest 30 --number 10 --sort age --save /etc/pacman.d/mirrorlist"
+
+#Recent Installed Packages
+alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+alias riplong="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -3000 | nl"
+
+#Cleanup orphaned packages
+alias cleanup='sudo pacman -Rns $(pacman -Qtdq);yay -Sc --noconfirm'
+
+alias cleanut='yay -Sc --noconfirm'
+#search content with ripgrep
+alias rg="rg --sort path"
+
+#get the error messages from journalctl
+alias jctl="journalctl -p 3 -xb"
+
+#vim for important configuration files
+#know what you do in these files
+alias vlightdm="sudoedit $EDITOR /etc/lightdm/lightdm.conf"
+alias vpacman="sudoedit $EDITOR /etc/lightdm/lightdm.conf"
+alias vpacman="sudoedit $EDITOR /etc/pacman.conf"
+alias vgrub="sudoedit $EDITOR /etc/default/grub"
+alias vconfgrub="sudoedit $EDITOR /boot/grub/grub.cfg"
+alias vmkinitcpio="sudoedit $EDITOR /etc/mkinitcpio.conf"
+alias vmirrorlist="sudoedit $EDITOR /etc/pacman.d/mirrorlist"
+alias vsddm="sudoedit $EDITOR /etc/sddm.conf"
+alias vsddmk="sudoedit $EDITOR /etc/sddm.conf.d/kde_settings.conf"
+alias vfstab="sudoedit $EDITOR /etc/fstab"
+alias vnsswitch="sudoedit $EDITOR /etc/nsswitch.conf"
+alias vsamba="sudoedit $EDITOR /etc/samba/smb.conf"
+alias vgnupgconf="sudoedit $EDITOR /etc/pacman.d/gnupg/gpg.conf"
+alias vb="$EDITOR ~/.bashrc"
+alias vz="$EDITOR ~/.zshrc"
+
+#gpg
+#verify signature for isos
+alias gpg-check="gpg2 --keyserver-options auto-key-retrieve --verify"
+alias fix-gpg-check="gpg2 --keyserver-options auto-key-retrieve --verify"
+#receive the key of a developer
+alias gpg-retrieve="gpg2 --keyserver-options auto-key-retrieve --receive-keys"
+alias fix-gpg-retrieve="gpg2 --keyserver-options auto-key-retrieve --receive-keys"
+alias fix-keyserver="[ -d ~/.gnupg ] || mkdir ~/.gnupg ; cp /etc/pacman.d/gnupg/gpg.conf ~/.gnupg/ ; echo 'done'"
+
+#systeminfo
+alias probe="sudo -E hw-probe -all -upload"
+alias sysfailed="systemctl list-units --failed"
+
+#shutdown or reboot
+alias ssn="sudo shutdown now"
+alias sr="sudo reboot"
+
+#give the list of all installed desktops - xsessions desktops
+alias xd="ls /usr/share/xsessions"
+alias nvchad="NVIM_APPNAME=nvchad nvim"
+alias lazyvim="NVIM_APPNAME=lazyvim nvim"
+alias astronvim="NVIM_APPNAME=astronvim nvim"
+alias expa='expac -H M "%011m\t%-20n\t%10d" (comm -23 (pacman -Qqen | sort|psub) (begin; pacman -Qqg xorg; expac -l \'\n\' \'%E\' base; end | sort -u|psub)) | sort -n'
+alias sudoe='sudo -E nvim'
